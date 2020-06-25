d51055c5b1a2b5f6a9a2fad27e0e67241dbf4f89
Hayk <hayk.hayr3@gmail.com>
2020-06-25 04:46:57 +0400

    Add project files.

---
+++ MetanitLesson1.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MetanitLesson1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Make your deposit: ");
+            decimal num = decimal.Parse(Console.ReadLine());
+
+            if(num > 0 && num < 100)
+            {
+                num += num * 5 / 100;
+            }
+            else if(num >= 100 && num <= 200)
+            {
+                num += num * 7 / 100;
+            }
+            else if(num > 200)
+            {
+                num += num * 10 / 100;
+            }
+            else
+            {
+                throw new Exception("Wrong deposit amount!");
+            }
+
+            Console.WriteLine($"The amount after adding the percent is {num}");
+
+            Console.ReadLine();
+        }
+    }
+}
